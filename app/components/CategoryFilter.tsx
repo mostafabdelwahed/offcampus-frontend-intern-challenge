043491657ca7bbd1f8711ea6535d66b9892184c6
Mostafa Abdelwahed <mostafabahaa258@gmail.com>
2026-06-10 09:34:05 +0300

    Added Category Filter

---
+++ app/components/CategoryFilter.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+interface Props {
+    returnCategory: (category: string) => void
+}
+
+export default function CategoryFilter({ returnCategory }: Props) {
+  const [category, setCategory] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setCategory(event.target.value as string);
+    returnCategory(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 2,
+        backgroundColor: 'background.paper',
+        borderBottom: '1px solid',
+        borderColor: 'divider'
+        }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={category}
+          label="Category"
+          onChange={handleChange}
+        >
+          <MenuItem value={"All"}>All</MenuItem>
+          <MenuItem value={"Internship"}>Internship</MenuItem>
+          <MenuItem value={"Student Club"}>Student Club</MenuItem>
+          <MenuItem value={"Part-time"}>Part-time</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import { useState } from "react";
 import OpportunityGrid from "./components/OpportunityGrid";
 import SearchBar from "./components/SearchBar";
-
-
+import CategoryFilter from "./components/CategoryFilter";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("All");
   return <>
     <SearchBar setSearchTerm={setSearchTerm} />
+    <CategoryFilter returnCategory={setCategory} />
     <OpportunityGrid />
   </>;
 }
