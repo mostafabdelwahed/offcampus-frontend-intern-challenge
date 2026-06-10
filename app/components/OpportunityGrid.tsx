5bd7843abd78be3ad59c2ca9b1576c194166d08d
Mostafa Abdelwahed <mostafabahaa258@gmail.com>
2026-06-10 09:48:49 +0300

    Implemented Searching and Category Filtering Functionalities

--- app/components/OpportunityGrid.tsx
+++ app/components/OpportunityGrid.tsx
@@ -1,14 +1,18 @@
-import { Opportunity, MOCK_DATA } from "../data/opportunities";
+import { Opportunity } from "../data/opportunities";
 import OpportunityCard from "./OpportunityCard";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 
-const OpportunityGrid = () => {
+interface Props {
+    opportunities: Opportunity[];
+}
+
+const OpportunityGrid = ({ opportunities }: Props) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        {MOCK_DATA.map((item: Opportunity) => (
+        {opportunities.map((item: Opportunity) => (
           <Grid size={12} key={item.id}>
             <OpportunityCard opportunity={item} />
           </Grid>
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,20 @@
 import { useState } from "react";
 import OpportunityGrid from "./components/OpportunityGrid";
 import FilterBar from "./components/FilterBar";
+import { Opportunity, MOCK_DATA } from "./data/opportunities";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
   const [category, setCategory] = useState("All");
+
+  const filteredOpportunities = MOCK_DATA.filter((opportunity) => {
+    const matchesSearch = opportunity.title.toLowerCase().includes(searchTerm.toLowerCase()) || opportunity.company.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchesCategory = category === "All" || opportunity.category === category;
+    return matchesSearch && matchesCategory;
+  });
+
   return <>
     <FilterBar returnSearchTerm={setSearchTerm} returnCategory={setCategory} />
-    <OpportunityGrid />
+    <OpportunityGrid opportunities={filteredOpportunities} />
   </>;
 }
